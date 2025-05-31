9bdb92e5b81d234ccdb383d39ac5992094e9fb7e
Wen Ow <ow.wen2022@gmail.com>
2025-05-31 12:39:44 -0400

    Improvements to main_app

--- watchlist_manager/main_app/urls.py
+++ watchlist_manager/main_app/urls.py
@@ -1,12 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('watchitems/', views.watchitem_index, name='watchitem_index'),
-    path('watchitems/<int:pk>/', views.watchitem_detail, name='watchitem_detail'),
-    path('watchitems/create/', views.WatchItemCreate.as_view(), name='watchitem_create'),
-    path('watchitems/<int:pk>/update/', views.WatchItemUpdate.as_view(), name='watchitem_update'),
-    path('watchitems/<int:pk>/delete/', views.WatchItemDelete.as_view(), name='watchitem_delete'),
 ]
 
